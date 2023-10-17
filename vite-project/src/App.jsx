cda962f65431b89d5957ce2df88eae8c2ed17b06
imgibo <gibscreative@gmail.com>
2023-10-17 16:20:58 +0800

    just made repository

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+function App() {
+  return <div>
+    <RouterProvider router={router} />
+  </div>
+}
+
+export default App
